0ff8c2c13381b276d309fd6380b6966276f7039c
Emily Aquin <eaquin1@gmail.com>
2020-03-19 18:57:11 -0500

    added random gif selection

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const removeBtn = document.getElementById('removebtn');
 inputBtn.addEventListener('click', async function (e) {
     e.preventDefault();
     const searchTerm = document.getElementById('searchinput').value;
-    console.log(searchTerm)
+  
     const gif =  await getGiphy(searchTerm);
-    console.log(gif)
+  
     appendImg(gif);
 });
 
@@ -18,8 +18,17 @@ removeBtn.addEventListener('click', function (){
 async function getGiphy(input) {
     const url = `http://api.giphy.com/v1/gifs/search?q=${input}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
     const res = await axios.get(url);
-    console.log(res.data.data[0].embed_url)
-    return res.data.data[0].embed_url;
+
+    //choose random gif from results
+    let numResults = res.data.data.length;
+  
+    if (numResults > 1) {
+        let randomIdx = Math.floor(Math.random() * numResults);
+        return res.data.data[randomIdx].images.original.url;
+        } else {
+            return res.data.data[0].images.original.url;
+        }
+    
 }
 
 
